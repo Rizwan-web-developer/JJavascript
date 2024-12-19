2a7eb658688c196226066a712917e3fa36c8de3a
Rizwan-web-developer <rk3113798@gmail.com>
2024-12-19 12:02:59 +0500

    working

---
+++ r.js
@@ -0,0 +1,82 @@
+// var let ,const,
+// Use for suppose
+// Example
+// let name ="Muhammad Rizwan "
+// let fatherName ="Abdul-Latif"
+// console.log (name + fatherName)
+
+// let rollNumber = 55
+// console.log (rollNumber)
+
+
+
+// let num1 = 55
+// let num2 = 55
+// console.log (num1+num2)
+
+// alert,confirm
+// alert
+// means hooshiar
+// example
+// alert ("Are you Human")
+
+// confirm
+// means pakka krna
+// Example
+// confirm ("Are you Ready")
+
+
+// let yourName = prompt("Enter Your Name")
+// console.log (yourName)
+
+// problem
+// document.write(studentName)
+
+
+
+
+// let num1 = +prompt ("Enter your Number")
+// let num2 = +prompt ("Enter your Number")
+// console.log (num1+num2)
+
+
+// let number1 = -prompt ("Enter your Number")
+// let number2 = -prompt ("Enter your Number")
+// console.log (num1-num2)
+
+// problem
+// let number1 = *prompt ("Enter your Number")
+// let number2 = *prompt ("Enter your Number")
+// console.log (num1*num2)
+
+
+// Array,object,function
+
+
+// objrct
+// let employee = {
+//     name: Rizwan,salary:50000,company:RizwanIndustries,github:Rizwan-web-developer,  School:North-star-public-secondary-school,
+// }
+// console.log(employee)
+
+
+// Array
+// let details = [Rizwan,Abdul-latif,]
+// console.log (details)
+
+
+// functions
+// function name (){
+// console.log ("Muhammad Rizwan")
+// console.log ("Abdul latif")
+// console.log ("Salary 5000")
+// console.log ("company")
+
+// }
+// name()
+
+// function addnumber (num1,num2){
+//     console.log (num1+num2)
+// }
+
+// addnumber(50,50)
